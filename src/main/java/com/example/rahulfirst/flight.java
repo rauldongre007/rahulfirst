aac5f00da25b3068bc74ec3a8946519585cb3896
ritu bashire <britu1992@gmail.com>
2022-12-21 19:13:14 +0530

    hello1

--- src/main/java/com/example/rahulfirst/flight.java
+++ src/main/java/com/example/rahulfirst/flight.java
@@ -3,8 +3,8 @@ package com.example.rahulfirst;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-public class bus {
-    @GetMapping("/bus")
-    public String getData() { return "red bus booking";}
+public class flight {
+    @GetMapping("/flight")
+    public String getData() { return "AEROPLANE travel";}
 
 }
